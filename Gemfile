7605a17ba3ffe9217c2812af72a08a25620751f3
Meagan Waller <meagan@meaganwaller.com>
2024-10-20 15:38:58 -0400

    Add new favicon and livereload

--- Gemfile
+++ Gemfile
@@ -47,6 +47,10 @@ gem "google-cloud-storage", "~> 1.8", require: false
 
 gem "simplecov", require: false, group: :test
 
+group :development do
+  gem "hotwire-livereload"
+end
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri windows], require: "debug/prelude"
---
+++ app/javascript/controllers/sidebar_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static targets = ['menu']
+
+  open (event) {
+    event.preventDefault()
+    this.menuTarget.classList.add('active')
+  }
+
+  close (event) {
+    event.preventDefault()
+    this.menuTarget.classList.remove('active')
+  }
+
+  handleCache = () => {
+    this.menuTarget.classList.remove('active')
+  }
+
+  connect() {
+    document.addEventListener('turbo:before-cache', this.handleCache)
+  }
+
+  disconnect() {
+    document.removeEventListener('turbo:before-cache', this.handleCache)
+  }
+}
