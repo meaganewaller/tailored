1a80169521f2e1bba14ac3558f5d83d7bbb3f134
Meagan Waller <meagan@meaganwaller.com>
2024-10-21 10:18:41 -0400

    WIP

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,27 +1,16 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['menu']
+  static targets = ['sidebarContainer']
 
-  open (event) {
-    event.preventDefault()
-    this.menuTarget.classList.add('active')
+  toggle(e) {
+    e.preventDefault();
+    this.switchCurrentState();
   }
 
-  close (event) {
-    event.preventDefault()
-    this.menuTarget.classList.remove('active')
-  }
-
-  handleCache = () => {
-    this.menuTarget.classList.remove('active')
-  }
-
-  connect() {
-    document.addEventListener('turbo:before-cache', this.handleCache)
-  }
-
-  disconnect() {
-    document.removeEventListener('turbo:before-cache', this.handleCache)
+  switchCurrentState() {
+    const newState = this.element.dataset.expanded === 'true' ? 'false' : 'true'
+    this.element.dataset.expanded = newState
+    document.cookie = `sidebar_expanded=${newState}`
   }
 }
